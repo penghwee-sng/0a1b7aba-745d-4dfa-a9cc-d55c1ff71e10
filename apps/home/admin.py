4001f8f09275ed47c37ae0b1930e38e1957c7430
Peng Hwee <64183178+penghwee-sng@users.noreply.github.com>
2022-08-04 14:10:02 +0800

    Fix BookingAdmin again

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 from .models import Room, Booking
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display('datetime_start', 'booking_user', 'booking_room', 'scenario')
+    pass
 
 # Register your models here.
 admin.site.register(Booking, BookingAdmin)
